96dd06860f0bbabad8cc5d44979f8edfa438192d
solomiia-marko <soliamark25@gmail.com>
2023-08-23 02:48:55 +0300

    Lifted Up the Open State

--- src/App.js
+++ src/App.js
@@ -25,25 +25,30 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+
   return (
     <div className="accordion">
       {data.map((el, i) => (
         <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
           num={i + 1}
           title={el.title}
-          text={el.text}
           key={el.title}
-        ></AccordionItem>
+        >
+          {el.text}
+        </AccordionItem>
       ))}
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+  const isOpen = num === curOpen;
 
   function handleToggle() {
-    setIsOpen(isOpen => !isOpen);
+    onOpen(isOpen ? null : num);
   }
 
   return (
@@ -51,7 +56,7 @@ function AccordionItem({ num, title, text }) {
       <p className="number">{num < 9 ? `0${num}` : num}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
